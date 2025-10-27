0b821e36912e6da136ef286689536bf8f1fd5ba6
Fernando C. Lohmann <107650854+Lohmann12@users.noreply.github.com>
2025-10-27 21:13:01 +0000

    Nível mestre concluído

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,9 +5,9 @@ int main() {
     int tabuleiro[10][10];
     int tamanhoNavio = 3;
     
-    for(int coluna = 0; coluna < 10; coluna++) {
-        for(int linha = 0; linha < 10; linha++) {
-            tabuleiro[coluna][linha] = 0;
+    for(int linha = 0; linha < 10; linha++) {
+        for(int coluna = 0; coluna < 10; coluna++) {
+            tabuleiro[linha][coluna] = 0;
         }
     }
     
@@ -25,20 +25,90 @@ int main() {
     for (int diagonal = 0; diagonal < tamanhoNavio; diagonal++) {
         tabuleiro[2 + diagonal][8 - diagonal] = 3;
     }
+
+    int alcanceCone = 3;
+    int origemLinhaCone = 3;
+    int origemColunaCone = 7;
+
+    for (int distancia = 1; distancia <= alcanceCone; distancia++) {
+        int largura = distancia - 1;  // 🔥 Mude aqui!
+        for (int deslocamento = -largura; deslocamento <= largura; deslocamento++) {
+            int linha = origemLinhaCone - distancia;
+            int coluna = origemColunaCone + deslocamento;
+            if (linha >= 0 && linha < 10 && coluna >= 0 && coluna < 10) {
+                tabuleiro[linha][coluna] = 5;
+            } 
+        }
+    }
+
+    int alcanceCruz = 2;
+    int origemLinhaCruz = 3;
+    int origemColunaCruz = 3;
+
+    for (int distancia = 0; distancia <= alcanceCruz; distancia++){
+        int linhaCima = origemLinhaCruz - distancia;
+        int linhaBaixo = origemLinhaCruz + distancia;
+        if (linhaCima >= 0) {
+            tabuleiro[linhaCima][origemColunaCruz] = 1;
+        }
+        if (linhaBaixo < 10) {
+            tabuleiro[linhaBaixo][origemColunaCruz] = 1;
+        }
+
+        int colunaEsquerda = origemColunaCruz - distancia;
+        int colunaDireita = origemColunaCruz + distancia;
+        if (colunaEsquerda >= 0) {
+            tabuleiro[origemLinhaCruz][colunaEsquerda] = 1;
+        }
+        if (colunaDireita < 10) {
+            tabuleiro[origemLinhaCruz][colunaDireita] = 1;
+        }
+    }
+
+int alcanceOcta = 1;
+int origemLinhaOcta = 7;
+int origemColunaOcta = 6;
+
+for (int distancia = 0; distancia <= alcanceOcta; distancia++) {
+    int linhaCima = origemLinhaOcta - distancia;
+    int colunaCima = origemColunaOcta;
     
+    int linhaBaixo = origemLinhaOcta + distancia;
+    int colunaBaixo = origemColunaOcta;
+    
+    int linhaEsquerda = origemLinhaOcta;
+    int colunaEsquerda = origemColunaOcta - distancia;
+    
+    int linhaDireita = origemLinhaOcta;
+    int colunaDireita = origemColunaOcta + distancia;
+    
+    if (linhaCima >= 0) {
+        tabuleiro[linhaCima][colunaCima] = 4;
+    }
+    if (linhaBaixo < 10) {
+        tabuleiro[linhaBaixo][colunaBaixo] = 4;
+    }
+    if (colunaEsquerda >= 0) {
+        tabuleiro[linhaEsquerda][colunaEsquerda] = 4;
+    }
+    if (colunaDireita < 10) {
+        tabuleiro[linhaDireita][colunaDireita] = 4;
+    }
+}
+
     printf("  ");
     for(int linha = 0; linha < 10; linha++) {
         printf("%c ", colunas[linha]);
     }
-    printf("\n");
+    printf("\n"); //Numeração de A - J;
     
     for(int coluna = 0; coluna < 10; coluna++) {
         printf("%d ", coluna + 1);
         for(int linha = 0; linha < 10; linha++) {
             printf("%d ", tabuleiro[coluna][linha]);
         }
-        printf("\n");
+        printf("\n"); //Numeração de 1 - 10;
     }
-
+    
     return 0;
 }
